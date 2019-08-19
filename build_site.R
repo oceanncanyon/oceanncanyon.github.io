0522ae8f6bce24798a462171ff8c7016e9199abc
Warren <warren@cometnet-10-21-29-43.utdallas.edu>
2019-08-19 09:38:53 -0500

    0

---
+++ build_site.R
@@ -0,0 +1,10 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/warren/warrenwang_portfolio.github.io")
+
+#render your sweet site. 
+rmarkdown::render_site()
+#### generate html for r docs in the wd and initiatial setup file.
+
